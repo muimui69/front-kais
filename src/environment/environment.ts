7ae67e9bfd30011697f67178704f6797894b5eb8
JuniorZamoAdunay <86460391+junior-zamo99@users.noreply.github.com>
2026-01-08 08:27:42 -0400

    fix:errores v1

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    baseUrl: 'http://localhost:3000/api',
-  //baseUrl: 'http://192.168.88.73:3000/api'
+  //baseUrl: 'http://localhost:3000/api',
+  baseUrl: 'http://134.209.51.98:3000/api'
 }
