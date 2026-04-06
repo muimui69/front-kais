f6b7d15b80f273835f0f9be50501cabef838428b
JuniorZamoAdunay <86460391+junior-zamo99@users.noreply.github.com>
2026-04-06 13:44:45 -0400

    rutas v2

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   //baseUrl: 'http://localhost:3000/api',
   //imgUrl: 'http://localhost:3000',
-  baseUrl: 'http://api.tebusco.net/api',
-  imgUrl: 'http://api.tebusco.net.52'
+  baseUrl: 'https://api.tebusco.net/api',
+  imgUrl: 'https://api.tebusco.net'
   // baseUrl: 'http://134.209.51.98:3000/api',
   // imgUrl: 'http://134.209.51.98:3000'
 }
