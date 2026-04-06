3d404fc309fc2c23f561518ddff8cd52f3d39db7
JuniorZamoAdunay <86460391+junior-zamo99@users.noreply.github.com>
2026-04-06 13:20:14 -0400

    ssl

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   //baseUrl: 'http://localhost:3000/api',
   //imgUrl: 'http://localhost:3000',
-  baseUrl: 'http://66.94.104.52/api',
-  imgUrl: 'http://66.94.104.52'
+  baseUrl: 'http://api.tebusco.net/api',
+  imgUrl: 'http://api.tebusco.net.52'
   // baseUrl: 'http://134.209.51.98:3000/api',
   // imgUrl: 'http://134.209.51.98:3000'
 }
